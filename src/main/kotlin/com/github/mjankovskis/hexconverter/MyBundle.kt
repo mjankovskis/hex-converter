4c7bfa44e05a226c830089cb0f487a2601fe0d60
GitHub Action <action@github.com>
2022-09-13 08:09:26 +0000

    Template cleanup

--- src/main/kotlin/com/github/mjankovskis/hexconverter/MyBundle.kt
+++ src/main/kotlin/com/github/mjankovskis/hexconverter/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.mjankovskis.hexconverter
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/mjankovskis/hexconverter/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/mjankovskis/hexconverter/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.mjankovskis.hexconverter.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.mjankovskis.hexconverter.MyBundle
 
 class MyApplicationService {
 
